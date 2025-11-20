d049955db357c6bec58e218436c3c8aee1c4ab82
lanoriskyfauzi-maker <lanoriskyfauzi@gmail.com>
2025-11-20 20:09:37 +0700

    Update ultah.js

--- ultah.js
+++ ultah.js
@@ -225,13 +225,14 @@ function startTypewriter() {
     
     const fullMessage = `Hi bibih,
 
-Happy Birthday! Selamat ulang tahun yang ke 21 yachhh😘😘
+Happy Birthday! Selamat ulang tahun yang ke 21 yaa😘
 
-Hari ini aku pengen kamu ngerasain semua hal positif dan keajaiban yang cuma bisa didapetin kalo kamu ada di dunia ini. Semoga segala keinginanmu tercapai (termasuk beli hp bareng aku yaa🙈), apalagi yang kocak-kocak dan gak biasa, karena kamu tuh unik banget! Aku selalu percaya kalau kamu bisa melewati semua tantangan dengan kekuatan dan semangat yang luar biasa.
+Hari ini aku pengen kamu ngerasain semua hal positif dan keajaiban yang cuma bisa didapetin kalo kamu ada di dunia ini. Semoga segala keinginanmu tercapai(termasuk beli hp bareng aku🙈), apalagi yang kocak-kocak dan gak biasa, karena kamu tuh unik banget! Aku selalu percaya kalau kamu bisa melewati semua tantangan dengan kekuatan dan semangat yang luar biasa.
 
-Terima kasih udah jadi bagian hidup aku yang paling berharga. Kamu bener-bener bikin hari-hari aku jadi lebih berarti dan penuh warna. Semoga di tahun yang baru ini, kamu makin bahagia, makin sukses, makin dewasa, berfikir secara kritis, dan tentunya makin cantik (walaupun udah cantik banget sih!).
+Terima kasih udah jadi bagian hidup aku yang paling berharga. Kamu bener-bener bikin hari-hari aku jadi lebih berarti dan penuh warna. Semoga di tahun yang baru ini, kamu makin bahagia, makin sukses, makin dewasa, makin bisa kontrol emosi dan tentunya makin cantik (walaupun udah cantik banget sih!).
 
 I love you so much! 💕`;
+    
     
     // Clear content and start fresh
     messageContent.innerHTML = '';
@@ -266,7 +267,7 @@ function skipTypewriter() {
         clearInterval(typewriterInterval);
         const messageContent = document.querySelector('.message-content');
         if (messageContent) {
-            const fullMessage = `Hi Cel,<br><br>Happy Birthday!<br><br>Hari ini aku pengen kamu ngerasain semua hal positif dan keajaiban yang cuma bisa didapetin kalo kamu ada di dunia ini. Semoga segala keinginanmu tercapai, apalagi yang kocak-kocak dan gak biasa, karena kamu tuh unik banget! Aku selalu percaya kalau kamu bisa melewati semua tantangan dengan kekuatan dan semangat yang luar biasa.<br><br>Terima kasih udah jadi bagian hidup aku yang paling berharga. Kamu bener-bener bikin hari-hari aku jadi lebih berarti dan penuh warna. Semoga di tahun yang baru ini, kamu makin bahagia, makin sukses, dan tentunya makin cantik (walaupun udah cantik banget sih!).<br><br>I love you so much! 💕`;
+            const fullMessage =`Hi bibih,<br><br>Happy Birthday! selamat ulang tahun yang ke 21 yaa😘<br><br>Hari ini aku pengen kamu ngerasain semua hal positif dan keajaiban yang cuma bisa didapetin kalo kamu ada di dunia ini. Semoga segala keinginanmu tercapai(termasuk beli hp bareng aku🙈), apalagi yang kocak-kocak dan gak biasa, karena kamu tuh unik banget! Aku selalu percaya kalau kamu bisa melewati semua tantangan dengan kekuatan dan semangat yang luar biasa.<br><br>Terima kasih udah jadi bagian hidup aku yang paling berharga. Kamu bener-bener bikin hari-hari aku jadi lebih berarti dan penuh warna. Semoga di tahun yang baru ini, kamu makin bahagia, makin sukses, makin dewasa ,makin bisa kontrol emosi dan tentunya makin cantik (walaupun udah cantik banget sih!).<br><br>I love you so much! 💕`;
             messageContent.innerHTML = fullMessage;
             isTyping = false;
             messageContent.scrollTop = messageContent.scrollHeight;
@@ -1192,5 +1193,6 @@ function handleContinueNavigation() {
     }
 
 }
+
 
 
